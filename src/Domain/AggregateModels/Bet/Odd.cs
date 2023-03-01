852ebcf59488a6a23902538678c0f360d1993cdd
Motinha <motinha11@outlook.pt>
2023-03-01 18:43:44 +0000

    HA-101: Complete odd value object;

--- src/Domain/AggregateModels/Bet/Odd.cs
+++ src/Domain/AggregateModels/Bet/Odd.cs
@@ -11,6 +11,7 @@ namespace ArbitrageFinder.Domain.AggregateModels.Bet
 {
     using System;
     using System.Collections.Generic;
+    using ArbitrageFinder.Domain.AggregateModels.Bet.Enum;
     using ArbitrageFinder.Domain.SeedWork;
 
     /// <summary>
@@ -25,11 +26,13 @@ namespace ArbitrageFinder.Domain.AggregateModels.Bet
         /// <param name="oddId">The odd identifier.</param>
         /// <param name="bookmakerId">The bookmaker identifier.</param>
         /// <param name="realOdd">The real odd.</param>
-        public Odd(Guid oddId, Guid bookmakerId, decimal realOdd)
+        /// <param name="type">The type.</param>
+        public Odd(Guid oddId, Guid bookmakerId, decimal realOdd, OddType type)
         {
             this.OddId = oddId;
             this.BookmakerId = bookmakerId;
             this.RealOdd = realOdd;
+            this.Type = type;
         }
 
         /// <summary>
@@ -49,6 +52,12 @@ namespace ArbitrageFinder.Domain.AggregateModels.Bet
         /// </summary>
         /// <value>The real odd.</value>
         public decimal RealOdd { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>The type.</value>
+        public OddType Type { get; set; }
 
         /// <summary>
         /// Gets the atomic values.
